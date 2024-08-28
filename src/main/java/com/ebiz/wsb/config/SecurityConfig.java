16c6bd35c8bceb4a9685902667339aa72b2512a4
onehowon <flash0221@naver.com>
2024-08-28 23:34:22 +0900

    add : Spring Security 설정 build : 빌드 파일 수정

---
+++ src/main/java/com/ebiz/wsb/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.ebiz.wsb.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable()).authorizeHttpRequests((auth) -> {
+                    auth.anyRequest().permitAll();
+                }
+        );
+        return http.build();
+    }
+}
---
+++ src/main/java/com/ebiz/wsb/domain/auth/api/AuthController.java
@@ -0,0 +1,10 @@
+package com.ebiz.wsb.domain.auth.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+}
