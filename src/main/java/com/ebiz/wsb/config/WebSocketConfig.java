b7d510b2adc06355240f3e156648db2aaae003b7
onehowon <flash0221@naver.com>
2024-09-30 12:23:01 +0900

    feat: 웹소켓 활용 실시간 출석 정보 반영

--- src/main/java/com/ebiz/wsb/config/WebSocketConfig.java
+++ src/main/java/com/ebiz/wsb/config/WebSocketConfig.java
@@ -2,13 +2,19 @@ package com.ebiz.wsb.config;
 
 
 import com.ebiz.wsb.domain.location.application.StompHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 @EnableWebSocketMessageBroker
 @Configuration
@@ -16,6 +22,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final StompHandler stompHandler;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config){
@@ -33,5 +40,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws")
                 .setAllowedOrigins("*");
                 // .withSockJS();
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        // MappingJackson2MessageConverter에 ObjectMapper를 설정하여 LocalDateTime을 직렬화 가능하게 함
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        objectMapper.registerModule(new JavaTimeModule());  // ObjectMapper에 JavaTimeModule 등록
+        converter.setObjectMapper(objectMapper);
+        messageConverters.add(converter);
+        return false;  // 기본 메시지 컨버터들을 유지하도록 설정
     }
 }
---
+++ src/main/java/com/ebiz/wsb/domain/attendance/api/AttendanceWebSocketController.java
@@ -0,0 +1,29 @@
+package com.ebiz.wsb.domain.attendance.api;
+
+import com.ebiz.wsb.domain.attendance.application.AttendanceService;
+import com.ebiz.wsb.domain.attendance.dto.AttendanceDTO;
+import com.ebiz.wsb.domain.attendance.dto.AttendanceStatusDTO;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class AttendanceWebSocketController {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final AttendanceService attendanceService;
+
+
+    @MessageMapping("/attendance/update")
+    public void updateAttendanceWebSocket(Long attendanceId, AttendanceStatusDTO attendanceStatusDTO) {
+
+        AttendanceDTO updatedAttendance = attendanceService.updateAttendance(attendanceId, attendanceStatusDTO);
+
+        log.info("출석 상태 업데이트: 학생 ID={}, 상태={}", updatedAttendance.getStudentId(), updatedAttendance.getStatus());
+
+        messagingTemplate.convertAndSend("/sub/attendance/updates", updatedAttendance);
+    }
+}
