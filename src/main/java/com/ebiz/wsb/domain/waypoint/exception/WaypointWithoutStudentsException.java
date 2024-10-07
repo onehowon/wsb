3c5b6de166a65e6448cbd3c470586b88343752fc
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2024-10-07 23:09:41 +0900

    feat: 경유지 별 학생 조회 기능 구현 및 일부 수정

---
+++ src/main/java/com/ebiz/wsb/domain/waypoint/exception/WaypointWithoutStudentsException.java
@@ -0,0 +1,7 @@
+package com.ebiz.wsb.domain.waypoint.exception;
+
+public class WaypointWithoutStudentsException extends RuntimeException {
+    public WaypointWithoutStudentsException(String message) {
+        super(message);
+    }
+}
