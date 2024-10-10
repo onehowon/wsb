751d8500ccca8468ed92b6f32a9c8f6b5ef7c952
jinyoung1226 <jinyoung1226@ajou.ac.kr>
2024-10-11 05:19:57 +0900

    feat: 웹소캣 출결 변경 구현

--- src/main/java/com/ebiz/wsb/domain/attendance/api/AttendanceController.java
+++ src/main/java/com/ebiz/wsb/domain/attendance/api/AttendanceController.java
@@ -4,21 +4,23 @@ import com.ebiz.wsb.domain.attendance.application.AttendanceService;
 import com.ebiz.wsb.domain.attendance.dto.AttendanceUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/attendance")
 public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
-    @PostMapping("/update")
-    public ResponseEntity<String> updateAttendance(@RequestBody AttendanceUpdateRequest request) {
-        attendanceService.updateAttendance(request.getStudentId(), request.getAttendanceStatus());
-        return ResponseEntity.ok("출석 상태가 업데이트되었습니다.");
+    @MessageMapping("/sub/group/{groupId}")
+    public void updateAttendance(@Payload AttendanceUpdateRequest request, @DestinationVariable Long groupId) {
+        attendanceService.updateAttendance(request.getStudentId(), groupId , request.getAttendanceStatus());
     }
 
 }
