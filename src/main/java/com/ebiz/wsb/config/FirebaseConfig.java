20e0683ac87eb3d10815a208579e0505cc9bf0e7
onehowon <flash0221@naver.com>
2024-10-28 15:38:20 +0900

    fix: firebase 활용 푸시 알림 기능 설정 수정

--- src/main/java/com/ebiz/wsb/config/FirebaseConfig.java
+++ src/main/java/com/ebiz/wsb/config/FirebaseConfig.java
@@ -1,27 +1,29 @@
 package com.ebiz.wsb.config;
 
+import com.google.api.client.util.Value;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("${firebase.config.path}")
+    private String firebaseConfigPath;
+
     @PostConstruct
-    public void initializeFirebase() {
-        try (InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("firebase/firebase-adminsdk.json")) {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-        } catch (IOException e) {
-            throw new IllegalStateException("Firebase 설정 초기화 실패", e);
-        }
+    public void initializeFirebase() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath);
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        FirebaseApp.initializeApp(options);
     }
 }
